037944085aa9812f6879155d4dbfd7f3e36709b8
DobrevaI <iidobreva24a@gmail.com>
2026-02-17 14:08:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
-books = ["The A.B.C. Murders", "Everyone on this train is a suspect", "The great Gatsby", "Nineteen Eighty-Four", "1984", "To Kill a Mockingbird", "The Hobbit"]
+books = [
+  "The A.B.C. Murders",
+  "Everyone on this train is a suspect",
+  "The great Gatsby",
+  "Nineteen Eighty-Four",
+  "1984",
+  "To Kill a Mockingbird",
+  "The Hobbit"]
 st.title("Book checker app")
 st.write("Enter a book title to check if it's it the database")
 user_input=st.text_input("Book title")
@@ -10,3 +17,8 @@ if st.button("Check book"):
     st.success("The book exists in the database")
   else:
     st.error("The book is not in the database")
+
+
+new_book=st.text_input("Add book")
+if st.button("Add")
+st.write(new_book)
